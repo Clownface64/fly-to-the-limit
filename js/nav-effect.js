a78f8568068ee38087fb32c5fc94d50ce885e0ec
Brendon Bryon-Kay <brendon_kay@hotmail.com>
2016-04-15 10:55:16 +1200

    added JavaScript

---
+++ js/nav-effect.js
@@ -0,0 +1,20 @@
+// Find the nav bar
+var navBar = document.querySelector('nav');
+
+// Do something when the user scrolls
+window.onscroll = function(event) {
+
+	// If the distance from the top is 200px
+	if( document.body.scrollTop >= 200 ) {
+
+		// Hide the nav
+		navBar.className = 'nav-hide';
+
+	} else {
+
+		// Show the nav
+		navBar.className = 'nav-fixed';
+
+	}
+
+}
